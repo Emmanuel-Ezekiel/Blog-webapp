0545edcb78fb7b136ed09b8d8cd793971dbc004b
Emmanuel-Ezekiel <emamex44@gmail.com>
2021-09-25 15:42:27 +0100

    add all files

---
+++ Desktop/startbootstrap-clean-blog-gh-pages/index.js
@@ -0,0 +1,33 @@
+const express = require ('express')
+const app = express()
+
+app.use(express.static('public'))
+
+const path = require ('path')
+const ejs = require('ejs')
+app.set('view engine','ejs')
+
+app.get('/',(req,res)=>{
+// res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+res.render('index');
+})
+
+app.get('/about',(req,res)=>{
+// res.sendFile(path.resolve(__dirname,'pages/about.html'))
+res.render('about');
+})
+
+app.get('/contact',(req,res)=>{
+// res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+res.render('contact');
+})
+
+app.get('/post',(req,res)=>{
+// res.sendFile(path.resolve(__dirname,'pages/post.html'))
+res.render('post');
+})
+
+app.listen(4000, ()=>{
+console.log('App listening on port 4000')
+})
+
